d4072987c2d7229dc86a7d7e8792c76c7200429e
Deborah Kaplan <deborahgu@users.noreply.github.com>
2017-10-23 13:11:15 -0400

    Changing the license to AGPL 3 per request.  (the license modification is acceptable because there are no downloads  of the library and it hasn't been advertised  to anyone; the Apache license  was essentially on pre-alpha code.)

--- abbyy_to_epub3/tests/test_parse_abbyy.py
+++ abbyy_to_epub3/tests/test_parse_abbyy.py
@@ -1,4 +1,21 @@
 ﻿# -*- coding: utf-8 -*-
+# Copyright 2017 Deborah Kaplan
+#
+# This file is part of Abbyy-to-epub3.
+# Source code is available at <https://github.com/deborahgu/abbyy-to-epub3>.
+#
+# Abbyy-to-epub3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from ebooklib import epub
 import pytest
