253704ad8ecb0b7d291e438f5f2eb3b8def9f1c9
Michael E. Karpeles (Mek) <michael.karpeles@gmail.com>
2018-03-29 20:01:49 +0000

    adding devpi upload sh

---
+++ upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Only works within IA's network.
+# Make sure to bump version # in __init__.py prior to running
+
+pip3 install devpi-client
+devpi use https://devpi.archive.org/
+devpi login books
+devpi use books/formats
+devpi upload
+pip3 install --upgrade -i https://devpi.archive.org/books/formats abbyy_to_epub3
+
+
